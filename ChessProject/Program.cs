2d108380bb32318ddc9d4d25dc699c392b21d79f
Ramos <joaopedroramos1512@gmail.com>
2026-02-14 21:25:53 -0300

    Creating Position Class

--- ChessProject/Program.cs
+++ ChessProject/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace ChessProject
+﻿using Chess.Board;
+
+namespace Chess
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Position P;
+
+            P = new(3,5);
+
+            System.Console.WriteLine("Posição: " + P);
+            System.Console.ReadLine();
         }
     }
 }
